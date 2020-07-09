b88c8b8264ba202f09021c23c83955228fb30878
Andrey Yamanov <tenphi@gmail.com>
2020-07-09 14:56:03 +0300

    remove unused code

--- src/pages/home.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <nu-block>
-    <nu-h1>Home page</nu-h1>
-  </nu-block>
-</template>
--- src/pages/second.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <nu-block>
-    <nu-h1>Second page</nu-h1>
-  </nu-block>
-</template>
